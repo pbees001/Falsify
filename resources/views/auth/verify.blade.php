d53c5b78e74fca39c47d7a469b1a55dd3987afe6
Arun Tella <aruntella@Aruns-Air.fios-router.home>
2021-10-05 00:08:10 -0400

    Updated README along with views

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -5,21 +5,15 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header">{{ __('Admin needs to approve your account') }}</div>
 
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            {{ __('Your account request has been successfully submitted to admin.') }}
                         </div>
                     @endif
-
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
-                    <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
-                        @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
-                    </form>
+                        {{ __('You can use it once approved by admin.') }}
                 </div>
             </div>
         </div>
