c8605d7ba8ae76c9d267de9ea8610b911b588bfd
pbees001 <pbees001@odu.edu>
2021-12-08 10:59:05 -0500

    Add extra information in footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -4,6 +4,7 @@
         height: 8vh;
         bottom: 0;
         box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.15) !important;
+        line-height: 0.1;
     }
     .footer-style{
         color: #0eaad6;
@@ -12,8 +13,18 @@
         alignment: center;
         padding-top: 15px;
     }
+    .footer-style-copyright{
+        color: #0eaad6;
+        text-align: right;
+        font-size: 10px;
+        alignment: right;
+        padding-right: 15px;
+    }
 
 </style>
 <footer>
     <p class="footer-style">Falsify</p>
+    <p class="footer-style-copyright">Copyright 2021</p>
+    <p class="footer-style-copyright">@author: <i>Priyatham</i></p>
+
 </footer>
